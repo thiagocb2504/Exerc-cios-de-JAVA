4c5171afc2dd9d4145ef9a220edebc73c6e5dc6b
Thiago Brito <thiagodacostab2504@gmail.com>
2025-04-03 11:44:58 -0300

    PessoaTeste.java

--- oo/encapsulamento/PessoaTeste.java
+++ oo/encapsulamento/PessoaTeste.java
@@ -4,9 +4,12 @@ public class PessoaTeste {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa();
-		//p1.idade = -30; // alteramos o valor da variavel
+		Pessoa p1 = new Pessoa("Pedro", " Amoedo", -45);
+		p1.setIdade (230); // alteramos o valor da variavel
 		
-		System.out.println(p1.idade); // lemos o valor da variavel
+		System.out.println(p1.getIdade()); // lemos o valor da variavel
+		System.out.println(p1); // lemos o valor da variavel
+		System.out.println(p1.getNomeCompleto());
 	}
 }
+
